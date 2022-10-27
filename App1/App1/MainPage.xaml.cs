72782040877b658af4359e795d10f42169e87433
Kamil Oles <kamiloles14@gmail.com>
2022-10-27 11:36:37 +0200

    Lokalizacja

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void Button_Clicked(object sender , EventArgs e)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.GetLocationAsync(request);
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+                if (location == null)
+                {
+                    LabelLocation.Text = "No Gps";
+                }
+                else
+                {
+                    LabelLocation.Text = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+
+        }
+    }
+}
